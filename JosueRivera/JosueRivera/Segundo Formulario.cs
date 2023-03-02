b1bdf02d7366783ccb8b64e0b067084d73db2d07
Josue Rivera <jm.rivera@unah.hn>
2023-03-02 13:41:27 -0600

    Primer Commit
    Tercer Formulario

---
+++ JosueRivera/JosueRivera/Segundo Formulario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JosueRivera
+{
+    public partial class Segundo_Formulario : Form
+    {
+        public Segundo_Formulario()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ JosueRivera/JosueRivera/Tercer Formulario.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JosueRivera
+{
+    public partial class Tercer_Formulario : Form
+    {
+        public Tercer_Formulario()
+        {
+            InitializeComponent();
+        }
+
+        private string nombre;
+        private string apellido;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = 0;
+            listBox1.Items.Clear();
+            numerosde1al100(num);
+        }
+
+        private void numerosde1al100(int numero)
+        {
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    nombre = "Josue";
+                    apellido = "Rivera";
+                    listBox1.Items.Add(nombre + " " + apellido);
+                }
+                else if (i % 3 == 0)
+                {
+                    nombre = "Josue";
+                    listBox1.Items.Add(nombre);
+                }
+                else if (i % 5 == 0)
+                {
+                    apellido = "Rivera";
+                    listBox1.Items.Add(apellido);
+                }
+                else
+                {
+                    listBox1.Items.Add(i);
+                }
+
+
+            }
+
+        }
+
+    }
+}
